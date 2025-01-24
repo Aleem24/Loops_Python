7d7393e28a177a499bf1e2acda27680464da1765
Aleem24 <md.samiulaleem16@gmail.com>
2025-01-24 17:10:14 +0600

    Pattern of a prime

--- pattern.py
+++ pattern.py
@@ -1,14 +1,28 @@
-# for i in range(1,11):
-#     for j in range(1,11):
-#         print("* ",end="")
-#     print()
+
 
 
 
 # Right angle pattern
-row = int(input("Enter the number of rows:"))
+# row = int(input("Enter the number of rows:"))
 
-for i in range(1,row+1):
-    for j in range(i):
-        print("*",end="")
-    print()
+# for i in range(10,row+1):
+#     for j in range(i):
+#         print("* ",end="")
+#     print()
+
+#Take input from user
+
+num = int(input("Enter the number:"))
+
+# workings of prime or composite checker
+
+if num > 1:
+    for i in range (2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is not a prime number ")
+        break
+    else:
+        print(f"{num} is a prime number ")
+else:
+        print(f"{num} is not a prime number ")
+ 
